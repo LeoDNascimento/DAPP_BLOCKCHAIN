583d52dfcbb32dcc435fb80b032e8173160c982f
LeoDNascimento <leonardo.nascimento1306@gmail.com>
2022-12-12 06:34:38 -0300

    Push

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -14,177 +14,131 @@ import { Button, Divider } from "@mui/material";
 import TextField from "@mui/material/TextField";
 
 function Home() {
-  const [name, setName] = useState("");
-  const [cpf, setCpf] = useState("");
-  const [cpfSearch, setCpfSearch] = useState("");
-  const [owner, setOwner] = useState("");
-  const [birthdate, setBirthdate] = useState("");
-  const [people, setPeople] = useState([]);
-  const [personInfo, setPersonInfo] = useState(null);
+  const [candidateNames, setCandidateNames] = useState([]);
+  const [validCandidate, setValidCandidate] = useState("");
+  const [vote, setVote] = useState("");
   const [loadingInsert, setLoadingInsert] = useState(false);
-
-  // Pega dono do contrato
-  const getOwner = async () => {
+  
+  const getCandidates = async () => {
     try {
-      const _owner = await registry.methods.owner().call();
-
-      setOwner(_owner);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  // Pega pessoas cadastradas no contrato
-  const getPeople = async () => {
-    try {
-      const response = await registry.methods.getAllPeople().call();
+      const response = await registry.methods.getAllCandidates().call();
       console.log(response);
-      setPeople(response);
+      setCandidateNames(response);
+      
     } catch (error) {
       console.log(error);
     }
   };
-  // Busca pessoa por cpf
-  const handleSearchPerson = async (e) => {
-    e.preventDefault();
-    try {
-      // const response = await registry.methods
-      //   .findByCpf(cpfSearch)
-      //   .call();
-      for (let i = 0; i < people.length; i++) {
-        console.log(people[i].cpf);
-        if (people[i].cpf === cpfSearch) {
-          setPersonInfo(people[i]);
-          break;
-        }
-        else{
-          setPersonInfo(null);
-        }
-      }
-     
-   
-    } catch (error) {
-      setPersonInfo(null);
-      console.log(error);
-    }
-  };
-  // Cadastra nova pessoa
-  const handleRegistry = async (e) => {
-    e.preventDefault();
-    try {
-      setLoadingInsert(true);
 
+  const handleViewVotes = async (e) => {
+    e.preventDefault();
+    try {
       const contas = await web3.eth.getAccounts();
       const response = await registry.methods
-        .registry([name, parseInt(cpf), parseInt(birthdate)])
+        .totalVotesFor(vote)
         .send({ from: contas[0] });
 
       console.log(response);
-      setLoadingInsert(false);
+
     } catch (error) {
-      setLoadingInsert(false);
       alert("Ops, erro no cadastro de pessoa");
       console.log(error);
     }
   };
-  // Busca dono do contrato e lista de pessoas no inicio da página e quando um novo dado é inserido
-  useEffect(() => {
-    getOwner();
-    getPeople();
-  }, [loadingInsert]);
+
+  const handleValidCandidate = async(e) => {
+    e.preventDefault();
+    try {
+      console.log(candidateNames)
+      const contas = await web3.eth.getAccounts();
+      const response = await registry.methods
+        .voteForCandidate(validCandidate)
+        .send({ from: contas[0] });
+
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  // Cadastra nova pessoa
+  const handleRegisterCandidate = async (e) => {
+    e.preventDefault();
+    try {
+
+      console.log(candidateNames)
+      const contas = await web3.eth.getAccounts();
+      const response = await registry.methods
+        .registerCandidates([candidateNames])
+        .send({ from: contas[0] });
+
+      console.log(response);
+
+    } catch (error) {
+      alert("Ops, erro no cadastro de pessoa");
+      console.log(error);
+    }
+  };
+
+  const add = async(e) =>{
+    e.preventDefault();
+    try {
+      setCandidateNames([...candidateNames, candidateNames])
+
+    } catch (error) {
+      alert("Ops, erro no cadastro de pessoa");
+      console.log(error);
+    }
+    
+    
+  }
+  // useEffect(() => {
+  //   getCandidates();
+  //   console.log(candidateNames)
+  // }, []);
   return (
     <div>
-      <h1>Endereço do dono do contrato: {owner}</h1>
-      <Divider />
-      <h2>Listagem de Pessoas Registradas</h2>
-      <TableContainer>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>ID</TableCell>
-              <TableCell align="right">Nome</TableCell>
-              <TableCell align="right">CPF</TableCell>
-              <TableCell align="right">Data de Nascimento</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {people.map((person, idx) => (
-              <TableRow
-                key={idx}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {idx + 1}
-                </TableCell>
-                <TableCell align="right">{person.name}</TableCell>
-                <TableCell align="right">{person.cpf}</TableCell>
-                <TableCell align="right">{person.birthdate}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-
-      </TableContainer>
-      {!(people.length > 0) ? <h4>Nenhuma pessoa cadastrada</h4> : null}
-
-      <br />
-      <br />
+      <h2>Listagem de Candidatos</h2>
+      
       <Divider />
 
-      <form onSubmit={handleRegistry}>
+      <form onSubmit={handleRegisterCandidate}>
         <div className="content">
-          <h2>Cadastro de pessoa</h2>
-          <TextField id="outlined-basic" label="Nome" variant="outlined" value={name} onChange={(e) => setName(e.target.value)} />
-          <br />
-          <br />
-          <TextField id="outlined-basic" label="CPF" variant="outlined" type="number" value={cpf} onChange={(e) => setCpf(e.target.value)} />
-          <br />
-          <br />
-          {/* tipe number */}
-          <TextField id="outlined-basic" label="Data de Nascimento" variant="outlined"  type="number" value={birthdate} onChange={(e) => setBirthdate(e.target.value)} />
+          <h2>Cadastro de Candidatos</h2>
+          <TextField id="outlined-basic" label="Candidate" variant="outlined" value={candidateNames} onChange={(e)=> add(e.target.value)} />
           <br />
           <br />
           <Button variant="contained" type="submit">Cadastrar</Button>
-          {loadingInsert ? (
-            <h5>Processando cadastro de Pessoas...</h5>
-          ) : (
-            <>
-              <br />
-              <br />
-            </>
-          )}
         </div>
       </form>
+      
 
       <Divider />
-      <form onSubmit={handleSearchPerson}>
+
+      <form onSubmit={handleValidCandidate}>
         <div className="content">
-          <h2>Buscar por cpf</h2>
-
-          <TextField id="outlined-basic" label="CPF" variant="outlined" value={cpfSearch} onChange={(e) => setCpfSearch(e.target.value)} />
-
+          <h2>Candidato Válido</h2>
+          <TextField id="outlined-basic" label="Candidate" variant="outlined" value={validCandidate} onChange={(e)=>setValidCandidate(e.target.value)} />
           <br />
           <br />
-          <Button variant="contained" type="submit">Buscar</Button>
-          
-          {personInfo ? (
-            <div>
-              <p>
-                <b>Nome:</b> {personInfo.name}
-              </p>
-              <p>
-                <b>CPF:</b> {personInfo.cpf}
-              </p>
-              <p>
-                <b>Data de nascimento:</b> {personInfo.birthdate}
-              </p>
-            </div>
-          ) : <h4>Não encontrado</h4>}
+          <Button variant="contained" type="submit">Votar</Button>
         </div>
       </form>
 
       <br />
       <br />
+      <Divider />
+
+      <form onSubmit={handleViewVotes}>
+        <div className="content">
+          <h2>Total de Votos para</h2>
+          <TextField id="outlined-basic" label="Candidate" variant="outlined" value={vote} onChange={(e)=>setVote(e.target.value)} />
+          <br />
+          <br />
+          <Button variant="contained" type="submit">Olhar</Button>
+        </div>
+      </form>
     </div>
+    
   );
 }
 
